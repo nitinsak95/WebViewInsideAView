2295901dc3f898f35051cefc243141c8169c23ed
affixusimac1 <viveking@outlook.com>
2018-01-22 15:54:13 +0530

    Initial Commit

---
+++ webKitView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  webKitView
+//
+//  Created by AFFIXUS IMAC1 on 1/22/18.
+//  Copyright © 2018 AFFIXUS IMAC1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
