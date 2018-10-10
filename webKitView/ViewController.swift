fdc0007747d8b3cfbcddae66b2ec75accd263d96
affixusimac1 <viveking@outlook.com>
2018-10-10 13:04:42 +0530

    dd

--- webKitView/ViewController.swift
+++ webKitView/ViewController.swift
@@ -7,19 +7,21 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webview: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let url = URL(string: "https://www.google.com")
+        let request = URLRequest(url: url!)
+        
+        webview.load(request)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override var prefersStatusBarHidden: Bool{
+        return true
     }
-
-
 }
 
